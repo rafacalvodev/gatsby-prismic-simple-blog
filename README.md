5b7fba5da6f75cc60d2e3c8c91a8b878a67fd150
rafacalvo <rafael.calvo.gonzalez@gmail.com>
2021-03-25 19:20:32 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## Prismic & Gatsby.js Practice Blog
+
+Project made as practice to see if I can make and deploy something made with Prismic and Gatsby. Made after following [this](https://prismic.io/docs/technologies/sample-blog-gatsby) tutorial.
---
+++ prismic-configuration.js
@@ -0,0 +1,11 @@
+module.exports = {
+  // The repo name for your Prismic repository (found in the url of your repo: https://gatsby-practice-2.prismic.io/)
+  prismicRepo: 'gatsby-practice-2',
+
+  // Release ID to build (requires access)
+  releaseID: 'YFtT7BEAACQATGDZ',
+
+  // access token to access the release
+  accessToken:
+    'MC5ZRnRUN0JFQUFDVUFUR0Rh.e2Rg77-977-977-977-9Fe-_ve-_ve-_vVrvv73vv73vv73vv71K77-9A--_vRnvv70w77-9Xu-_vXnvv73vv73vv700CQ',
+}
